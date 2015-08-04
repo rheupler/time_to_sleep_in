0ffdac054669fc3380bb919e112116a38cf8f37b
Julianna Green & Ryan Heupler <student@epicodus.com>
2015-08-04 15:21:04 -0700

    passing specs

--- lib/time_to_sleep_in.rb
+++ lib/time_to_sleep_in.rb
@@ -2,14 +2,25 @@ class String
 
   define_method(:time_to_sleep_in) do
     nametoday = ""
-    today = Time.new(2015, 8, 8)
-    if today.saturday?()
+    today = Time.new(self)
+    if today.wday() == 6
       nametoday.concat("Sleep in!")
-    elsif today.sunday?()
+    elsif today.wday() == 7
       nametoday.concat("Sleep in!")
+    elsif today.wday() == 1
+        nametoday.concat("Monday")
+    elsif today.wday() == 2
+        nametoday.concat("Tuesday")
+    elsif today.wday() == 3
+      nametoday.concat("Wednesday")
+    elsif today.wday() == 4
+      nametoday.concat("Thursday")
+    elsif today.wday() == 5
+      nametoday.concat("Friday")
    else
       false
     end
+    nametoday
   end
 end
 
